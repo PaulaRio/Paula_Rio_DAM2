91b553d4eea7fef883a2e9118645b8aa29b2d896
PaulaRio <paulariodelgadogay@gmail.com>
2024-10-21 17:47:06 +0200

    Intento divisas

---
+++ WPF_Divisas/Utils.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPF_Divisas
+{
+    public static class Utils
+    {
+        public static int GetNumeroPorConsola(string? numero)
+        {
+
+            if (!int.TryParse(numero, out int val))
+            {
+                Console.WriteLine("No has introducido un número");
+                return 0;
+            }
+            return val;
+        }
+        public static decimal cambioDivisas(decimal numero,string?de, string? a)
+        {   
+            //if(de)
+
+
+            return 0;
+        }
+
+
+
+
+    }
+}
