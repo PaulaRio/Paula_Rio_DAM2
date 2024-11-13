206f0b27500507fe863d82f179013f157ce8c74c
Paula <paulariodelgadogaygmail.com>
2024-11-13 08:44:56 +0100

    Ejemplo creación directorio por defecto

--- Archivos/Program.cs
+++ Archivos/Program.cs
@@ -5,15 +5,18 @@ using System.Linq;
 string root = Directory.GetParent(Directory.GetCurrentDirectory()).Parent.Parent.FullName;
 Console.WriteLine(Directory.GetCurrentDirectory());
 Console.WriteLine(root);
-string splitRandomFile =Path.GetRandomFileName().Replace(".","")+ ".txt";
-
+string nameRandomDirectory = Path.GetRandomFileName().Replace(".", "");
 
 if (!Directory.Exists(root + "/FILES"))
 { //Probar mejor a creae un string random con longitud limitada y hacer concat con .txt
     Directory.CreateDirectory(root + "/FILES");
-    
-    File.Create(root + "/FILES/"+ splitRandomFile);
-
+    for (int i = 0; i < 2; i++)
+    {
+        string nameRandomFile = Path.GetRandomFileName().Replace(".", "") + ".txt";
+        File.Create(root + "/FILES/" + nameRandomFile);
+    }
+       
+    Directory.CreateDirectory(root + "/FILES/"+ nameRandomDirectory);
 }
 else
 {
