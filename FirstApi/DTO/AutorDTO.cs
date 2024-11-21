7b552f51651aff1e016ce04b33987744e9a39333
PaulaRio <paulariodelgadogay@gmail.com>
2024-11-21 21:42:49 +0100

    Recordatorio en AutorDTO

--- FirstApi/DTO/AutorDTO.cs
+++ FirstApi/DTO/AutorDTO.cs
@@ -1,7 +1,7 @@
 ﻿namespace FirstApi.DTO
 {
     public class AutorDTO
-    {
+    {//Quitar stack que luego da error
         public string Nombre { get; set; }
         public string TELF { get; set; }
         public List<int> Libros { get; set; }
