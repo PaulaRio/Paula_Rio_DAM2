f35caba1f7dccaafc02282138840a93982a4c33f
PaulaRio <paulariodelgadogay@gmail.com>
2024-12-09 21:08:44 +0100

    Recuperar DTO

---
+++ Pokemon/DTO/HistoricoDTO.cs
@@ -0,0 +1,17 @@
+﻿namespace Pokemon.DTO
+{
+    public class HistoricoDTO
+    { 
+        public DateTime DateStart { get; set; }
+        public DateTime DateEnd { get; set; }
+        public string PokeName { get; set; }  
+        public int  DamageDoneTrainer { get; set; }
+        public int DamageReceivedTrainer { get; set; }
+        public int DamageDonePokemon { get; set; }
+        public bool Catch { get; set; }
+        public bool Shiny { get; set; }
+
+
+
+    }
+}
