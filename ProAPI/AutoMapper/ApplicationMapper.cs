621bc8f8de4e20013570dda63ffcbf8829a7f655
PaulaRio <paulariodelgadogay@gmail.com>
2025-01-09 21:37:19 +0100

    Eliminar archivos sobrantes.

---
+++ ProAPI/AutoMapper/ApplicationMapper.cs
@@ -0,0 +1,16 @@
+﻿using ApiPelicula.Models.DTOs.UserDto;
+using AutoMapper;
+
+using RestAPI.Models.Entity;
+
+namespace ApiPelicula.AutoMapper
+{
+    public class ApplicationMapper : Profile
+    {
+        public ApplicationMapper()
+        {
+            
+            CreateMap<AppUser, UserDto>().ReverseMap();
+        }
+    }
+}
---
+++ ProAPI/Data/ApplicationDbContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using RestAPI.Models.Entity;
+
+namespace ApiPelicula.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<AppUser>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+        //Add models here
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<AppUser> AppUsers { get; set; }
+      
+
+    }
+}
