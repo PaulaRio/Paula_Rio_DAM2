6ef54d91dd082d898aea0409a7231eed7dbf1a31
PaulaRioDelgado <163323519+PaulaRio@users.noreply.github.com>
2025-01-12 13:36:16 +0100

    Intento uso ui

---
+++ BasicApp/ViewModel/MainViewModel.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CommunityToolkit.Mvvm.Input;
+
+namespace BasicApp.ViewModel
+{
+    public partial class MainViewModel : ViewModelBase
+    {
+        private ViewModelBase? _selectedViewModel;
+        public MainViewModel(LoginViewModel login)
+        {
+            LoginViewModel = login;
+            SelectedViewModel = login;
+
+
+        }
+        public ViewModelBase? SelectedViewModel
+        {
+            get => _selectedViewModel;
+            set
+            {
+                SetProperty(ref _selectedViewModel, value);
+            }
+        }
+
+        public LoginViewModel LoginViewModel { get; }
+        
+
+        public async override Task LoadAsync()
+        {
+            if (SelectedViewModel is not null)
+            {
+                await SelectedViewModel.LoadAsync();
+            }
+        }
+        [RelayCommand]
+        private async void SelectViewModel(object? parameter)
+        {
+            SelectedViewModel = parameter as ViewModelBase;
+            await LoadAsync();
+        }
+
+
+
+    }
+}
+
