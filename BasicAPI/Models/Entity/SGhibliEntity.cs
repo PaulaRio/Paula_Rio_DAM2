ea33a646205deedbf8fa0bc0f2c07e7299e2b861
PaulaRioDelgado <163323519+PaulaRio@users.noreply.github.com>
2025-01-22 13:49:59 +0100

    Creación DataView y Entity

---
+++ BasicAPI/Models/Entity/SGhibliEntity.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace BasicAPI.Models.Entity
+{
+    public class SGhibliEntity
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string Name { get; set; }
+        public DateTime ReleaseDate { get; set; }
+        [Required]
+        public int Damage { get; set; }
+
+        [Required, MaxLength(50)]
+        public string Bastidor { get; set; }
+    }
+}
---
+++ BasicApp/ViewModel/MainViewModel.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CommunityToolkit.Mvvm.Input;
+
+namespace BasicApp.ViewModel
+{
+    public partial class MainViewModel : ViewModelBase
+    {
+        private ViewModelBase? _selectedViewModel;
+        public MainViewModel()
+        {
+            LoginViewModel = new LoginViewModel(this);
+            RegistrationViewModel = new RegistrationViewModel(this);
+            SelectedViewModel = LoginViewModel;
+
+
+        }
+        public ViewModelBase? SelectedViewModel
+        {
+            get => _selectedViewModel;
+            set
+            {
+                SetProperty(ref _selectedViewModel, value);
+            }
+        }
+
+        public LoginViewModel LoginViewModel { get; set; }
+        public RegistrationViewModel RegistrationViewModel { get; set; }
+
+
+        public async override Task LoadAsync()
+        {
+            if (SelectedViewModel is not null)
+            {
+                await SelectedViewModel.LoadAsync();
+            }
+        }
+        [RelayCommand]
+        private async void SelectViewModel(object? parameter)
+        {
+            SelectedViewModel = parameter as ViewModelBase;
+            await LoadAsync();
+        }
+
+
+
+    }
+}
+
