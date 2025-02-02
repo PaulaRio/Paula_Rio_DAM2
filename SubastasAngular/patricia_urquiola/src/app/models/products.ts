775f8c32ebd352fd8f62b89bc1dfffaad9bb28c5
PaulaRioDelgado <163323519+PaulaRio@users.noreply.github.com>
2025-02-02 23:17:53 +0100

    Subasta proyecto

---
+++ SubastasAngular/patricia_urquiola/src/app/models/products.ts
@@ -0,0 +1,10 @@
+export interface HousingLocation {
+    id: number;
+    name: string;
+    brand: string;
+    year: Date;
+    description: string;
+    photo: string;
+    max_bid: number;
+    
+}
