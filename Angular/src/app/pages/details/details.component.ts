12742ae8c90aa6737b679817acd97aafb16e268d
PaulaRioDelgado <163323519+PaulaRio@users.noreply.github.com>
2025-02-02 21:56:36 +0100

    Pasos

---
+++ Angular/src/app/pages/details/details.component.ts
@@ -0,0 +1,35 @@
+import { ActivatedRoute } from '@angular/router';
+import { HousingLocation } from 'src/app/models/housinglocation';
+import { HousingService } from 'src/app/services/housing.service';
+import {FormControl, FormGroup, ReactiveFormsModule} from '@angular/forms';
+import { Component, inject } from '@angular/core';
+
+@Component({
+  selector: 'app-details',
+  imports: [ReactiveFormsModule],
+  templateUrl: './details.component.html',
+  styleUrls: ['./details.component.css']
+})
+export class DetailsComponent {
+  route: ActivatedRoute = inject(ActivatedRoute);
+  applyForm = new FormGroup({
+    firstName: new FormControl(''),
+    lastName: new FormControl(''),
+    email: new FormControl(''),
+  });
+  
+  housingLocation: HousingLocation | undefined;
+  housingService: HousingService;
+  constructor(housingService: HousingService) {
+      const housingLocationId = Number(this.route.snapshot.params['id']);
+      this.housingLocation = housingService.getHousingLocationById(housingLocationId);
+      this.housingService=housingService;
+  }
+  submitApplication() {
+    this.housingService.submitApplication(
+      this.applyForm.value.firstName ?? '',
+      this.applyForm.value.lastName ?? '',
+      this.applyForm.value.email ?? '',
+    );
+  }
+}
