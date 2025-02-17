75f25b3ae80347f24e6b21cc7fe740ddf4252f67
PaulaRioDelgado <163323519+PaulaRio@users.noreply.github.com>
2025-02-17 13:23:30 +0100

    Pop up aniadido

---
+++ BasicApp/ViewModel/FeebackPropuestaViewModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CommunityToolkit.Mvvm.Input;
+using System.Windows;
+
+namespace BasicApp.ViewModel
+{
+    public partial class FeebackPropuestaViewModel : ViewModelBase
+    {
+        public Window? CurrentWindow { get; set; }
+
+        [RelayCommand]
+        private void Back_Click()
+        {
+            CurrentWindow?.Close();
+        }
+
+        [RelayCommand]
+        private void Send_Click()
+        {
+
+
+
+
+        }
+    }
+}
--- BasicApp/ViewModel/ValorarPropuestaViewModel.cs
+++ BasicApp/ViewModel/ValorarPropuestaViewModel.cs
@@ -3,10 +3,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using BasicApp.View;
+using CommunityToolkit.Mvvm.Input;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace BasicApp.ViewModel
 {
-    internal class ValorarPropuestaViewModel
+    public partial class ValorarPropuestaViewModel : ViewModelBase
     {
+
+        [RelayCommand]
+        private void Rechazar_Click()
+        {
+            var popUpWindow = App.Current.Services.GetService<AddView>();
+
+            popUpWindow?.Show();
+
+
+        }
     }
 }
