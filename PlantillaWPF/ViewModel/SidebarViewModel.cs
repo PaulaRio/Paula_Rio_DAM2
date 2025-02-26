8beb62275ef621431220fb60fe2944b8c08b3651
PaulaRioDelgado <163323519+PaulaRio@users.noreply.github.com>
2025-02-26 13:06:11 +0100

    Sidebar

---
+++ PlantillaWPF/ViewModel/SidebarViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlantillaWPF.ViewModel
+{
+    public partial class SidebarViewModel : ViewModelBase
+    {
+        public ObservableCollection<MenuItemModel> MenuItems { get; set; }
+        public ViewModelBase SelectedViewModel { get; set; }
+        public SidebarViewModel()
+        {
+            MenuItems = new ObservableCollection<MenuItemModel>
+        {
+            new MenuItemModel { Title = "Login", ViewModel = new LoginViewModel() },
+            new MenuItemModel { Title = "Register", ViewModel = new RegisterViewModel() },
+            new MenuItemModel { Title = "StackPanel View", ViewModel = new StackPanelViewModel() }
+        };
+
+            SelectedViewModel = MenuItems[0].ViewModel; // Selección inicial
+        }
+    }
+}
